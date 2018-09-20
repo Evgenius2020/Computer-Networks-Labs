824b236c4d829d886f6b2065fa6260765319dcf8
Evgenius2020 <spleen47@mail.ru>
2018-09-20 10:57:40 +0700

    01 Workable

--- 01. Multicast_Group/src/main.rs
+++ 01. Multicast_Group/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
 use std::thread;
@@ -17,22 +18,48 @@ fn main() {
         .join_multicast_v4(&multicast_addr, &local_addr)
         .expect("Join error");
 
-    let multicast_addr = SocketAddr::new(multicast_addr.into(), 1337);
-    let message = b"Hello from client!";
-    let mut buf = [0u8; 64];
-    
-    loop {    
-        socket
-            .send_to(message, multicast_addr)
-            .expect("Send error!");        
-        match socket.recv_from(&mut buf) {
-            Ok((len, remote_addr)) => {
-                let data = &buf[..len];
-                let response = String::from_utf8_lossy(data);
-                println!("Got data: {} from {}", response, remote_addr);
-            }
-            Err(_) => println!("Receive fail!"),
+    let socket_copy = socket.try_clone().expect("Failed to clone socket");
+    thread::spawn(move || {
+        let multicast_addr = SocketAddr::new(multicast_addr.into(), 1337);
+        let socket = socket_copy;
+        let message = b"Hello from client!";
+        loop {
+            socket
+                .send_to(message, multicast_addr)
+                .expect("Send error!");
+            thread::sleep(Duration::from_secs(1));
         }
-        thread::sleep(Duration::from_secs(1));
-    }
+    });
+
+    const ACTIVITY_DURATION: i16 = 3000;
+    thread::spawn(move || {
+        let mut buf = [0u8; 256];
+        let mut activity = HashMap::new();
+        let socket = socket.try_clone().expect("Failed to clone socket");
+        socket
+            .set_read_timeout(Some(Duration::from_millis(100)))
+            .expect("Failed to set read timeout");
+        loop {
+            match socket.recv_from(&mut buf) {
+                Ok((_, remote_addr)) => {
+                    activity.entry(remote_addr).or_insert(3000);
+                    activity.insert(remote_addr, ACTIVITY_DURATION);
+                }
+                Err(_) => { /* timeout */ }
+            }
+
+            print!("{}[2J", 27 as char);
+            let addrs = activity.iter_mut();
+            for (addr, last_seen) in addrs {
+                 *last_seen -= 50;
+                if *last_seen > 0 {
+                    println!("{} {}", addr, last_seen)
+                }
+            }
+
+            thread::sleep(Duration::from_millis(50));
+        }
+    });
+
+    loop {}
 }
