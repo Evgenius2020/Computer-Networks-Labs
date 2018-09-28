0ba8e4ca9c6ee948aa22baa589887e5eee1df5fe
evgenius2020 <spleen47@mail.ru>
2018-09-28 21:35:31 +0700

    02 Speed indicator

--- 02. TCP_File_Storage/Server/src/main.rs
+++ 02. TCP_File_Storage/Server/src/main.rs
@@ -1,15 +1,42 @@
 use std::cmp::min;
 use std::env;
-use std::fs::{File, DirBuilder};
+use std::fs::{DirBuilder, File};
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{SocketAddr, TcpListener};
+use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::Duration;
 
 fn read_line_without_delimeter(stream: &mut BufRead) -> String {
     let mut result = String::new();
     stream.read_line(&mut result).unwrap();
     result.pop();
     result
+}
+
+fn indicator(bytes_read: &Arc<Mutex<usize>>, client_addr: SocketAddr, file_size: usize) {
+    let bytes_read = Arc::clone(&bytes_read);
+    thread::spawn(move || {
+        const REFRESH_INTERVAL_SECS: u64 = 3;
+        let mut bytes_read_before: usize = 0;
+        loop {
+            thread::sleep(Duration::from_secs(REFRESH_INTERVAL_SECS));
+            let bytes_read = bytes_read.lock().unwrap();
+
+            println!(
+                "[{}]: {}/{} bytes received ({} b/s).",
+                client_addr,
+                *bytes_read,
+                file_size,
+                *bytes_read - bytes_read_before / REFRESH_INTERVAL_SECS as usize
+            );
+
+            if *bytes_read == file_size {
+                break;
+            }
+            bytes_read_before = *bytes_read;
+        }
+    });
 }
 
 fn main() {
@@ -41,19 +68,23 @@ fn main() {
             println!("[{}]: {} bytes left ...", client_addr, file_size);
 
             const BUF_SIZE: usize = 4096;
-            let mut bytes_read: usize = 0;
+            let bytes_read: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
+
+            indicator(&bytes_read, client_addr, file_size);
+
+            let bytes_read = Arc::clone(&bytes_read);
             {
                 let mut file =
                     File::create(format!("upload/{}", file_name)).expect("Failed to create file");
-                while bytes_read < file_size {
-                    let mut buf = vec![0u8; min(BUF_SIZE, file_size - bytes_read)];
+                loop {
+                    let mut bytes_read = bytes_read.lock().unwrap();
+                    let mut buf = vec![0u8; min(BUF_SIZE, file_size - *bytes_read)];
                     stream.read_exact(&mut buf).unwrap();
-                    bytes_read += buf.len();
-                    println!(
-                        "[{}]: {}/{} bytes received.",
-                        client_addr, bytes_read, file_size
-                    );
+                    *bytes_read += buf.len();
                     file.write_all(&buf).unwrap();
+                    if *bytes_read == file_size {
+                        break;
+                    }
                 }
             }
 
