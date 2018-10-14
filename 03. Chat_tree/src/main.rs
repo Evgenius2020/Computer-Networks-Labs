331a58cd04e84b891b8791be5413c763b558c015
evgenius2020 <spleen47@mail.ru>
2018-10-14 21:34:22 +0700

    03 Pretty output messages

--- 03. Chat_tree/src/main.rs
+++ 03. Chat_tree/src/main.rs
@@ -1,11 +1,11 @@
+extern crate rand;
+extern crate uuid;
+
 use std::env;
 use std::net::{SocketAddr, UdpSocket};
 use std::thread;
 use std::time::Duration;
-
-extern crate rand;
-use rand::*;
-extern crate uuid;
+use rand::Rng;
 use uuid::Uuid;
 // mod tree_node;
 // use self::tree_node::TreeNode;
@@ -37,9 +37,8 @@ fn main() {
     let socket = UdpSocket::bind(addr).expect("bind error");
     socket
         .set_read_timeout(Some(Duration::from_millis(100)))
-        .expect("set timeout errror");
+        .expect("set_read_timeout errror");
     let mut rand_generator = rand::thread_rng();
-
     let mut childs: Vec<SocketAddr> = Vec::new();
     if parent_addr.is_some() {
         childs.push(parent_addr.unwrap());
@@ -49,19 +48,19 @@ fn main() {
         let mut buf = [0u8; 16];
         match socket.recv_from(&mut buf) {
             Ok((_, src_addr)) => {
-                println!("received {}", Uuid::from_bytes(buf));
+                println!("Received '{}'", Uuid::from_bytes(buf));
                 let rand_number = rand_generator.gen_range(0, 100);
                 if rand_number > recv_fail_chance {
                     socket
                         .send_to("ok".as_bytes(), src_addr)
-                        .expect("send Ok error");
+                        .expect("sending 'ok' error");
 
                     if None == childs.iter().find(|&child| *child == src_addr) {
-                        println!("{} added", src_addr);
+                        println!("{} added to broadcasting group", src_addr);
                         childs.push(src_addr);
                     }
 
-                    childs = bcast(&buf, &socket, &mut childs, Some(src_addr));
+                    childs = broadcast(&buf, &socket, &mut childs, Some(src_addr));
                 }
             }
             Err(_) => { /* timeout */ }
@@ -70,14 +69,14 @@ fn main() {
         let rand_number = rand_generator.gen_range(0, 100);
         if rand_number > 90 {
             let message = Uuid::new_v4();
-            println!("bcasting {} ..", message);
-            childs = bcast(message.as_bytes(), &socket, &mut childs, None);
+            println!("Broadcasing '{}' started", message);
+            childs = broadcast(message.as_bytes(), &socket, &mut childs, None);
         }
         thread::sleep(Duration::from_millis(100));
     }
 }
 
-fn bcast(
+fn broadcast(
     message: &[u8],
     socket: &UdpSocket,
     childs: &mut Vec<SocketAddr>,
@@ -94,7 +93,7 @@ fn bcast(
             // println!("{} complete sent", child_addr);
             result.push(child_addr);
         } else {
-            println!("{} removed", child_addr);
+            println!("{} removed from broadcasting group", child_addr);
         }
     }
 
@@ -102,7 +101,7 @@ fn bcast(
 }
 
 fn send(socket: &UdpSocket, buf: &[u8], addr: &SocketAddr) -> bool {
-    socket.send_to(buf, addr).expect("send_to err");
+    socket.send_to(buf, addr).expect("send_to error");
     let mut confirmation_raw = [0u8; 16];
     let mut atempts_left = 3;
     loop {
@@ -119,7 +118,7 @@ fn send(socket: &UdpSocket, buf: &[u8], addr: &SocketAddr) -> bool {
             }
         }
 
-        println!("attemptions left {}", atempts_left);
+        println!("Attempts left {}", atempts_left);
         thread::sleep(Duration::from_millis(1000));
     }
 }
