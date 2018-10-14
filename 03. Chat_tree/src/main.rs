35139714387e3deb5fc9d3fc5b7f61d1d04d3936
evgenius2020 <spleen47@mail.ru>
2018-10-14 21:53:02 +0700

    03 tree_node

--- 03. Chat_tree/src/main.rs
+++ 03. Chat_tree/src/main.rs
@@ -7,8 +7,8 @@ use std::thread;
 use std::time::Duration;
 use rand::Rng;
 use uuid::Uuid;
-// mod tree_node;
-// use self::tree_node::TreeNode;
+mod tree_node;
+use self::tree_node::TreeNode;
 
 fn main() {
     let addr = env::args()
@@ -44,23 +44,25 @@ fn main() {
         childs.push(parent_addr.unwrap());
     }
 
+    let mut tree_node = TreeNode::new(socket, childs);
+
     loop {
         let mut buf = [0u8; 16];
-        match socket.recv_from(&mut buf) {
+        match tree_node.socket.recv_from(&mut buf) {
             Ok((_, src_addr)) => {
                 println!("Received '{}'", Uuid::from_bytes(buf));
                 let rand_number = rand_generator.gen_range(0, 100);
                 if rand_number > recv_fail_chance {
-                    socket
+                    tree_node.socket
                         .send_to("ok".as_bytes(), src_addr)
                         .expect("sending 'ok' error");
 
-                    if None == childs.iter().find(|&child| *child == src_addr) {
+                    if None == tree_node.childs.iter().find(|&child| *child == src_addr) {
                         println!("{} added to broadcasting group", src_addr);
-                        childs.push(src_addr);
+                        tree_node.childs.push(src_addr);
                     }
 
-                    childs = broadcast(&buf, &socket, &mut childs, Some(src_addr));
+                    tree_node.broadcast(&buf, Some(src_addr));
                 }
             }
             Err(_) => { /* timeout */ }
@@ -70,55 +72,8 @@ fn main() {
         if rand_number > 90 {
             let message = Uuid::new_v4();
             println!("Broadcasing '{}' started", message);
-            childs = broadcast(message.as_bytes(), &socket, &mut childs, None);
+            tree_node.broadcast(message.as_bytes(), None);
         }
         thread::sleep(Duration::from_millis(100));
-    }
-}
-
-fn broadcast(
-    message: &[u8],
-    socket: &UdpSocket,
-    childs: &mut Vec<SocketAddr>,
-    filter_addr: Option<SocketAddr>,
-) -> Vec<SocketAddr> {
-    let mut result = Vec::new();
-
-    for child_addr in childs.clone() {
-        if filter_addr.is_some() && child_addr == filter_addr.unwrap() {
-            result.push(child_addr);
-            continue;
-        }
-        if send(&socket, message, &child_addr) {
-            // println!("{} complete sent", child_addr);
-            result.push(child_addr);
-        } else {
-            println!("{} removed from broadcasting group", child_addr);
-        }
-    }
-
-    result
-}
-
-fn send(socket: &UdpSocket, buf: &[u8], addr: &SocketAddr) -> bool {
-    socket.send_to(buf, addr).expect("send_to error");
-    let mut confirmation_raw = [0u8; 16];
-    let mut atempts_left = 3;
-    loop {
-        match socket.recv_from(&mut confirmation_raw) {
-            Ok((_, _)) => {
-                return true;
-            }
-            Err(_) => {
-                /* timeout */
-                if atempts_left == 0 {
-                    return false;
-                }
-                atempts_left -= 1;
-            }
-        }
-
-        println!("Attempts left {}", atempts_left);
-        thread::sleep(Duration::from_millis(1000));
     }
 }
---
+++ 03. Chat_tree/src/tree_node.rs
@@ -0,0 +1,63 @@
+use std::net::{SocketAddr, UdpSocket};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+pub struct TreeNode {
+    pub socket: UdpSocket,
+    pub childs: Vec<SocketAddr>,
+}
+
+impl TreeNode {
+    pub fn new(socket: UdpSocket, childs: Vec<SocketAddr>) -> TreeNode {
+        TreeNode {
+            socket: socket,
+            childs: childs,
+        }
+    }
+
+    pub fn broadcast(&mut self, message: &[u8], filter_addr: Option<SocketAddr>) {
+        let mut result = Vec::new();
+
+        for child_addr in self.childs.clone() {
+            if filter_addr.is_some() && child_addr == filter_addr.unwrap() {
+                result.push(child_addr);
+                continue;
+            }
+
+            if self.send(message, &child_addr) {
+                // println!("{} complete sent", child_addr);
+                result.push(child_addr);
+            } else {
+                println!("{} removed from broadcasting group", child_addr);
+            }
+        }
+
+        self.childs = result;
+    }
+
+    fn send(&self, buf: &[u8], addr: &SocketAddr) -> bool {
+        self.socket.send_to(buf, addr).expect("send_to error");
+
+        let mut confirmation_raw = [0u8; 16];
+        let mut atempts_left = 3;
+        loop {
+            match self.socket.recv_from(&mut confirmation_raw) {
+                Ok((_, _)) => {
+                    return true;
+                }
+                Err(_) => {
+                    /* timeout */
+                    if atempts_left == 0 {
+                        return false;
+                    }
+                }
+            }
+
+            println!("Attempts left {}", atempts_left);
+            atempts_left -= 1;
+
+            thread::sleep(Duration::from_millis(1000));
+        }
+    }
+}
