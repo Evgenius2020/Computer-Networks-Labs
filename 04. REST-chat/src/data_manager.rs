1c6f66d0cbeddf136914c8793270786327fb0a3a
evgenius2020 <spleen47@mail.ru>
2018-11-14 02:21:27 +0700

    04 Messages

--- 04. REST-chat/src/data_manager.rs
+++ 04. REST-chat/src/data_manager.rs
@@ -1,29 +1,37 @@
-use UsersResult;
+use MessagesResult;
 use serde_json;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use uuid::Uuid;
 use LoginResult;
+use Message;
+use MessageSendRequest;
+use MessageSendResult;
 use User;
+use UsersResult;
 
 #[derive(Serialize, Deserialize)]
-pub struct UsersManager {
+pub struct DataManager {
     users: Vec<User>,
+    messages: Vec<Message>,
     tokens: HashMap<usize, String>,
-    next_id: usize,
+    users_next_id: usize,
+    messages_next_id: usize,
 }
 
-impl UsersManager {
-    pub fn new() -> UsersManager {
-        UsersManager {
+impl DataManager {
+    pub fn new() -> DataManager {
+        DataManager {
             users: Vec::new(),
+            messages: Vec::new(),
             tokens: HashMap::new(),
-            next_id: 1,
+            users_next_id: 1,
+            messages_next_id: 1,
         }
     }
 
-    pub fn load(filename: &str) -> Option<UsersManager> {
+    pub fn load(filename: &str) -> Option<DataManager> {
         match File::open(filename) {
             Ok(reader) => match serde_json::from_reader(reader) {
                 Ok(um) => Some(um),
@@ -43,19 +51,19 @@ impl UsersManager {
             Some(user_id) => match self.tokens.get(&user_id) {
                 Some(_) => None,
                 None => {
-                    self.tokens.insert(user_id, UsersManager::generate_uuid());
+                    self.tokens.insert(user_id, DataManager::generate_uuid());
                     Some((self.get_by_id(user_id).unwrap().clone(), false))
                 }
             },
             None => {
                 let user = User {
-                    id: self.next_id.clone(),
+                    id: self.users_next_id.clone(),
                     username: username.to_string(),
                     online: true,
                 };
-                self.tokens.insert(user.id, UsersManager::generate_uuid());
+                self.tokens.insert(user.id, DataManager::generate_uuid());
                 self.users.push(user.clone());
-                self.next_id += 1;
+                self.users_next_id += 1;
                 Some((user, true))
             }
         }
@@ -76,6 +84,35 @@ impl UsersManager {
 
     pub fn get_by_id(&mut self, id: usize) -> Option<&mut User> {
         self.users.iter_mut().nth(id)
+    }
+
+    pub fn add_message(&mut self, message: MessageSendRequest, token: &str) -> MessageSendResult {
+        let message = Message {
+            id: self.messages_next_id,
+            message: message.message,
+            author: self.get_id_by_token(token).unwrap(),
+        };
+        self.messages.push(message.clone());
+        self.messages_next_id += 1;
+
+        MessageSendResult {
+            id: message.id,
+            message: message.message,
+        }
+    }
+
+    pub fn generate_messages_result(&self, from: usize, to: usize) -> MessagesResult {
+        let mut result = MessagesResult {
+            messages: Vec::new()
+        };
+
+        for message in self.messages.iter() {
+            if message.id >= from || message.id <= to {
+                result.messages.push(message.clone());
+            }
+        }
+
+        result
     }
 
     pub fn logout(&mut self, token: &str) -> bool {
@@ -101,10 +138,10 @@ impl UsersManager {
 
     pub fn generate_users_result(&self) -> UsersResult {
         UsersResult {
-            users: self.users.clone()
+            users: self.users.clone(),
         }
     }
- 
+
     fn generate_uuid() -> String {
         Uuid::new_v4().to_string()
     }
--- 04. REST-chat/src/main.rs
+++ 04. REST-chat/src/main.rs
@@ -12,8 +12,8 @@ use hyper::service::service_fn;
 use hyper::{header, Body, Method, Request, Response, Server, StatusCode};
 use std::sync::{Arc, Mutex};
 
-mod users_manager;
-use users_manager::UsersManager;
+mod data_manager;
+use data_manager::DataManager;
 
 const FILENAME: &str = "target/db.json";
 
@@ -42,9 +42,32 @@ pub struct UsersResult {
     users: Vec<User>,
 }
 
+#[derive(Serialize)]
+pub struct MessagesResult {
+    messages: Vec<Message>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Message {
+    id: usize,
+    message: String,
+    author: usize,
+}
+
+#[derive(Deserialize)]
+pub struct MessageSendRequest {
+    message: String,
+}
+
+#[derive(Serialize)]
+pub struct MessageSendResult {
+    id: usize,
+    message: String,
+}
+
 type Fut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
-fn on_request_received(req: Request<Body>, um: Arc<Mutex<UsersManager>>) -> Fut {
+fn on_request_received(req: Request<Body>, dm: Arc<Mutex<DataManager>>) -> Fut {
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
@@ -54,18 +77,18 @@ fn on_request_received(req: Request<Body>, um: Arc<Mutex<UsersManager>>) -> Fut 
                 let login_request: LoginRequest =
                     serde_json::from_str(&String::from_utf8(body).unwrap()).unwrap();
 
-                let mut um = um.lock().unwrap();
+                let mut dm = dm.lock().unwrap();
 
-                match (*um).get_or_create(&login_request.username) {
+                match (*dm).get_or_create(&login_request.username) {
                     Some((user, created)) => {
                         if created {
-                            um.save(FILENAME);
+                            dm.save(FILENAME);
                         }
 
                         Response::builder()
                             .header(header::CONTENT_TYPE, "application/json")
                             .body(Body::from(
-                                serde_json::to_string(&um.generate_login_result(&user)).unwrap(),
+                                serde_json::to_string(&dm.generate_login_result(&user)).unwrap(),
                             )).unwrap()
                     }
                     None => Response::builder()
@@ -80,13 +103,13 @@ fn on_request_received(req: Request<Body>, um: Arc<Mutex<UsersManager>>) -> Fut 
             return Box::new(res);
         }
         (&Method::POST, "/logout") => {
-            let mut um = um.lock().unwrap();
-            let token = get_token(req);
-            match check_token(token.clone(), &um) {
+            let mut dm = dm.lock().unwrap();
+            let token = get_token(&req.into_parts().0);
+            match check_token(token.clone(), &dm) {
                 Some(res) => return Box::new(future::ok(res)),
                 None => {
-                    (*um).logout(&token.unwrap());
-                    um.save(FILENAME);
+                    (*dm).logout(&token.unwrap());
+                    dm.save(FILENAME);
                     let res = Response::builder()
                         .header(header::CONTENT_TYPE, "application/json")
                         .body(Body::from(r#"{ "message": "bye!" }"#))
@@ -96,22 +119,65 @@ fn on_request_received(req: Request<Body>, um: Arc<Mutex<UsersManager>>) -> Fut 
             }
         }
         (&Method::GET, "/users") => {
-            let mut um = um.lock().unwrap();
-            let token = get_token(req);
-            match check_token(token.clone(), &um) {
+            let mut dm = dm.lock().unwrap();
+            let token = get_token(&req.into_parts().0);
+            match check_token(token.clone(), &dm) {
                 Some(res) => return Box::new(future::ok(res)),
                 None => {
                     return Box::new(future::ok(
                         Response::builder()
                             .header(header::CONTENT_TYPE, "application/json")
                             .body(Body::from(
-                                serde_json::to_string(&(*um).generate_users_result()).unwrap(),
+                                serde_json::to_string(&(*dm).generate_users_result()).unwrap(),
                             )).unwrap(),
                     ))
                 }
             }
         }
+        (&Method::POST, "/messages") => {
+            let (head, body) = req.into_parts();
+            let token = get_token(&head);
+            let ch_tk = check_token(token.clone(), &dm.lock().unwrap());
+            match ch_tk {
+                Some(res) => return Box::new(future::ok(res)),
+                None => {
+                    let res = body.concat2().map(move |chunk| {
+                        let body = chunk.iter().cloned().collect::<Vec<u8>>();
+                        let message_send_request: MessageSendRequest =
+                            serde_json::from_str(&String::from_utf8(body).unwrap()).unwrap();
+                        let mut dm = dm.lock().unwrap();
 
+                        let message_send_result = &dm.add_message(message_send_request, &token.unwrap());
+                        dm.save(FILENAME);
+                        Response::builder()
+                            .header(header::CONTENT_TYPE, "application/json")
+                            .body(Body::from(
+                                serde_json::to_string(
+                                    message_send_result
+                                ).unwrap(),
+                            )).unwrap()
+                    });
+                    return Box::new(res);
+                }
+            }
+        }
+        (&Method::GET, "/messages") => {
+            let mut dm = dm.lock().unwrap();
+            let token = get_token(&req.into_parts().0);
+            match check_token(token.clone(), &dm) {
+                Some(res) => return Box::new(future::ok(res)),
+                None => {
+                    return Box::new(future::ok(
+                        Response::builder()
+                            .header(header::CONTENT_TYPE, "application/json")
+                            .body(Body::from(
+                                serde_json::to_string(&(*dm).generate_messages_result(0, 10))
+                                    .unwrap(),
+                            )).unwrap(),
+                    ))
+                }
+            }
+        }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
         }
@@ -120,8 +186,8 @@ fn on_request_received(req: Request<Body>, um: Arc<Mutex<UsersManager>>) -> Fut 
     Box::new(future::ok(response))
 }
 
-fn get_token(req: hyper::Request<hyper::Body>) -> Option<String> {
-    match req.into_parts().0.headers.get("Authorization") {
+fn get_token(head: &hyper::http::request::Parts) -> Option<String> {
+    match head.headers.get("Authorization") {
         Some(auth_header) => {
             let words = auth_header.to_str().unwrap().split(" ");
             if (words.clone().count() != 2) || (words.clone().nth(0).unwrap() != "Token") {
@@ -133,9 +199,9 @@ fn get_token(req: hyper::Request<hyper::Body>) -> Option<String> {
     }
 }
 
-fn check_token(token: Option<String>, um: &UsersManager) -> Option<hyper::Response<hyper::Body>> {
+fn check_token(token: Option<String>, dm: &DataManager) -> Option<hyper::Response<hyper::Body>> {
     match token {
-        Some(token) => match um.get_id_by_token(&token) {
+        Some(token) => match dm.get_id_by_token(&token) {
             Some(_) => None,
             None => Some(
                 Response::builder()
@@ -157,16 +223,16 @@ fn main() {
     let addr = ([127, 0, 0, 1], 1337).into();
 
     hyper::rt::run(future::lazy(move || {
-        let um = match UsersManager::load(FILENAME) {
-            Some(um) => um,
-            None => UsersManager::new(),
+        let dm = match DataManager::load(FILENAME) {
+            Some(dm) => dm,
+            None => DataManager::new(),
         };
-        let um = Arc::new(Mutex::new(um));
+        let dm = Arc::new(Mutex::new(dm));
 
         let server = Server::bind(&addr)
             .serve(move || {
-                let um = um.clone();
-                service_fn(move |req| on_request_received(req, um.clone()))
+                let dm = dm.clone();
+                service_fn(move |req| on_request_received(req, dm.clone()))
             }).map_err(|e| eprintln!("server error: {}", e));
 
         println!("Listening on http://{}", addr);
