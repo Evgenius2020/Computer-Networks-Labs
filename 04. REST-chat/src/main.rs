68d45e42c07725dd8d5dc72104dcc4423b3cbaf8
evgenius2020 <spleen47@mail.ru>
2018-11-13 20:50:06 +0700

    04 Login, logout

---
+++ 04. REST-chat/src/main.rs
@@ -0,0 +1,193 @@
+extern crate futures;
+extern crate hyper;
+extern crate uuid;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate url;
+
+use futures::future;
+use hyper::rt::{Future, Stream};
+use hyper::service::service_fn;
+use hyper::{header, Body, Method, Request, Response, Server, StatusCode};
+use std::sync::{Arc, Mutex};
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Clone)]
+struct User {
+    id: usize,
+    username: String,
+    online: bool,
+    token: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct LoginRequest {
+    username: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct UsersManager {
+    users: Vec<User>,
+    next_id: usize,
+}
+
+impl UsersManager {
+    pub fn new() -> UsersManager {
+        UsersManager {
+            users: Vec::new(),
+            next_id: 0,
+        }
+    }
+
+    pub fn get_or_create(&mut self, username: &str) -> Option<User> {
+        match self.get_id_by_name(username) {
+            Some(user_id) => {
+                let user = self.get_by_id(user_id).unwrap();
+                match user.token {
+                    Some(_) => None,
+                    None => {
+                        user.token = Some(Uuid::new_v4().to_string());
+                        Some(user.clone())
+                    }
+                }
+            }
+            None => {
+                let user = User {
+                    id: self.next_id.clone(),
+                    username: username.to_string(),
+                    online: true,
+                    token: Some(Uuid::new_v4().to_string()),
+                };
+                self.users.push(user.clone());
+                self.next_id += 1;
+                Some(user)
+            }
+        }
+    }
+
+    fn get_id_by_name(&self, username: &str) -> Option<usize> {
+        self.users.iter().position(|r| r.username == username)
+    }
+
+    fn get_id_by_token(&self, token: Option<String>) -> Option<usize> {
+        self.users.iter().position(|r| r.token == token)
+    }
+
+    pub fn get_by_id(&mut self, id: usize) -> Option<&mut User> {
+        self.users.iter_mut().nth(id)
+    }
+
+    pub fn logout(&mut self, token: &str) -> bool {
+        match self.get_id_by_token(Some(token.to_string())) {
+            Some(user_id) => {
+                let mut user = self.get_by_id(user_id).unwrap();
+                user.online = false;
+                user.token = None;
+                true
+            }
+            None => false,
+        }
+    }
+}
+
+type Fut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+
+fn on_request_received(req: Request<Body>, um: Arc<Mutex<UsersManager>>) -> Fut {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::POST, "/login") => {
+            let res = req.into_body().concat2().map(move |chunk| {
+                let body = chunk.iter().cloned().collect::<Vec<u8>>();
+                let login_request: LoginRequest =
+                    serde_json::from_str(&String::from_utf8(body).unwrap()).unwrap();
+
+                let mut um = um.lock().unwrap();
+
+                match (*um).get_or_create(&login_request.username) {
+                    Some(user) => Response::builder()
+                        .header(header::CONTENT_TYPE, "application/json")
+                        .body(Body::from(serde_json::to_string(&user).unwrap()))
+                        .unwrap(),
+                    None => Response::builder()
+                        .header(
+                            "WWW-Authenticate",
+                            "Token realm='Username is already in use'",
+                        ).status(StatusCode::UNAUTHORIZED)
+                        .body(Body::from(""))
+                        .unwrap(),
+                }
+            });
+            return Box::new(res);
+        }
+        (&Method::POST, "/logout") => {
+            let mut um = um.lock().unwrap();
+            let token = get_token(req);
+            match check_token(token.clone(), &um) {
+                Some(res) => return Box::new(future::ok(res)),
+                None => {
+                    (*um).logout(&token.unwrap());
+                    let res = Response::builder()
+                        .header(header::CONTENT_TYPE, "application/json")
+                        .body(Body::from(r#"{ "message": "bye!" }"#))
+                        .unwrap();
+                    return Box::new(future::ok(res));
+                }
+            }
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Box::new(future::ok(response))
+}
+
+fn get_token(req: hyper::Request<hyper::Body>) -> Option<String> {
+    match req.into_parts().0.headers.get("Authorization") {
+        Some(auth_header) => match auth_header.to_str().unwrap().split(" ").nth(1) {
+            Some(token) => Some(token.to_string()),
+            None => None,
+        },
+        None => None,
+    }
+}
+
+fn check_token(token: Option<String>, um: &UsersManager) -> Option<hyper::Response<hyper::Body>> {
+    match token {
+        Some(token) => match um.get_id_by_token(Some(token)) {
+            Some(_) => None,
+            None => Some(
+                Response::builder()
+                    .status(StatusCode::UNAUTHORIZED)
+                    .body(Body::from(""))
+                    .unwrap(),
+            ),
+        },
+        None => Some(
+            Response::builder()
+                .status(StatusCode::FORBIDDEN)
+                .body(Body::from(""))
+                .unwrap(),
+        ),
+    }
+}
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 1337).into();
+
+    hyper::rt::run(future::lazy(move || {
+        let um = Arc::new(Mutex::new(UsersManager::new()));
+
+        let server = Server::bind(&addr)
+            .serve(move || {
+                let um = um.clone();
+                service_fn(move |req| on_request_received(req, um.clone()))
+            }).map_err(|e| eprintln!("server error: {}", e));
+
+        println!("Listening on http://{}", addr);
+
+        server
+    }));
+}
