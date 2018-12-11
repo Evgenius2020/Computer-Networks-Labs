1048f41d3a029d977aa6252bcbee4b411b89b795
Evgenius2020 <spleen47@mail.ru>
2018-12-11 22:43:00 +0700

    05 Name-/Token- Login; reconnecting at client

--- 05. WebSocket/Client/js.js
+++ 05. WebSocket/Client/js.js
@@ -2,37 +2,62 @@ window.onload = () => {
     let login_button = document.getElementById("login-button");
     let logout_button = document.getElementById("logout-button");
     let username_input = document.getElementById("login-input");
+    let reconnect_button = document.getElementById("reconnect-button");
     let users_div = document.getElementById("users");
     let message_input = document.getElementById("message-input");
     let messages_div = document.getElementById("messages");
     let send_button = document.getElementById("send-button");
 
+    socket = null;
+    token = "";
     to_authorized(false);
+    connect();
 
-    let socket = new WebSocket("ws://192.168.0.104:1337")
-    socket.onmessage = (ev) => {
-        let resp = JSON.parse(ev.data);
-        let method = resp.method;
-        if (method == 'Login') {
-            let login_result = JSON.parse(resp.data);
-            if (login_result === null) {
-                return;
+    reconnect_button.onclick = (ev) => {
+        connect()
+    }
+
+    function connect() {
+        socket = new WebSocket("ws://127.0.0.1:1337");
+        socket.onmessage = (ev) => {
+            let resp = JSON.parse(ev.data);
+            let method = resp.method;
+            if (method == 'LoginResult') {
+                let login_result = JSON.parse(resp.data);
+                if (login_result === null) {
+                    return;
+                }
+
+                to_authorized(true);
+                token = login_result.token;
             }
+            if (method == "Messages") {
+                update_messages(JSON.parse(resp.data).messages)
+            }
+            if (method == "Users") {
+                update_users(JSON.parse(resp.data).users)
+            }
+        };
+        socket.onclose = (ev) => {
+            to_authorized(false);
+            reconnect_button.hidden = false;
+        }
 
-            to_authorized(true);
-            token = login_result.token;
-        }
-        if (method == "Messages") {
-            update_messages(JSON.parse(resp.data).messages)
-        }
-        if (method == "Users") {
-            update_users(JSON.parse(resp.data).users)
+        socket.onopen = (ev) => {
+            if (token !== "") {
+                socket.send(JSON.stringify({
+                    method: "TokenLogin",
+                    data: token
+                }))
+            }
+    
+            reconnect_button.hidden = true;
         }
     }
 
     login_button.onclick = () => {
         socket.send(JSON.stringify({
-            method: "Login",
+            method: "NameLogin",
             data: username_input.value
         }))
     }
@@ -54,7 +79,6 @@ window.onload = () => {
 
     function to_authorized(is_authorized) {
         if (!is_authorized) {
-            token = null;
             last_message_index = 0;
         }
         function disabled(isAuthorized) { return isAuthorized ? null : "disabled" };
